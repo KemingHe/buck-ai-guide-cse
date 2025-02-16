1cc21950fe3d0f933d4ad0f380927f05349a24fa
Keming He <keminghe.career@gmail.com>
2025-02-15 23:41:54 -0500

    build(pyproject.toml): updated poetry and deps
    updated poetry to v2, updated deps to match agentic-template, linted and formatted code, no behavior nor feature changes

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -24,10 +24,10 @@ from pinecone import Pinecone
 load_dotenv()
 
 # Retrieve the API keys from environment variables
-openai_api_key = getenv('OPENAI_API_KEY')
-openai_model_name = getenv('OPENAI_MODEL_NAME')
-pinecone_api_key = getenv('PINECONE_API_KEY')
-pintcone_index_name = getenv('PINECONE_INDEX_NAME')
+openai_api_key = getenv("OPENAI_API_KEY")
+openai_model_name = getenv("OPENAI_MODEL_NAME")
+pinecone_api_key = getenv("PINECONE_API_KEY")
+pintcone_index_name = getenv("PINECONE_INDEX_NAME")
 
 # ------------------------------------------------------------------------------
 # Initialize the OpenAI embeddings and the language model.
@@ -40,24 +40,22 @@ pc = Pinecone(api_key=pinecone_api_key)
 index = pc.Index(name=pintcone_index_name)
 vector_store = PineconeVectorStore(index=index, embedding=embeddings)
 
+
 # ------------------------------------------------------------------------------
 # Get the top relevant references from the Pinecone vector store.
 def get_relevant_refs(query):
     current_year: int = datetime.now().year
     refs = vector_store.similarity_search(
-        query=query,
-        k=5,
-        filter={
-            'lastmod_year': {'$gte': current_year - 1}
-        }
+        query=query, k=5, filter={"lastmod_year": {"$gte": current_year - 1}}
     )
     return refs
+
 
 # Get the response stream based on the user query.
 def get_response_stream(query, chat_history):
     relevant_refs = get_relevant_refs(query)
     relevant_docs = [ref.page_content for ref in relevant_refs]
-    template = '''
+    template = """
     You are an undergrad research advisor for the Computer Science and Engineering program at The Ohio State University. 
     You are helping students with their questions about general concepts, program adn career advise, and most importantly, research and faculty connections.
     You are given the chat history so far, the user question, and top relevant official info from the cse.osu.edu website.
@@ -69,54 +67,61 @@ def get_response_stream(query, chat_history):
     User question: {user_question}
 
     Relevent info: {relevent_info}
-    '''
+    """
     prompt = ChatPromptTemplate.from_template(template)
     chain = prompt | llm | StrOutputParser()
-    response_stream = chain.stream({
-        'chat_history': chat_history,
-        'user_question': query,
-        'relevent_info': '\n'.join(relevant_docs)
-    })
-    ref_urls = [ref.metadata['url'] for ref in relevant_refs]
+    response_stream = chain.stream(
+        {
+            "chat_history": chat_history,
+            "user_question": query,
+            "relevent_info": "\n".join(relevant_docs),
+        }
+    )
+    ref_urls = [ref.metadata["url"] for ref in relevant_refs]
     return response_stream, ref_urls
+
 
 # ------------------------------------------------------------------------------
 # Init the chat history.
-if 'chat_history' not in st.session_state:
+if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
 # ------------------------------------------------------------------------------
 # Streamlit page config.
-st.set_page_config(page_icon='🌰', page_title='Buck-AI-Guide')
+st.set_page_config(page_icon="🌰", page_title="Buck-AI-Guide")
 
 # Streamlit page content.
-st.title('🌰 💻 Buck-AI-Guide for CSE')
-st.info('Hi! I am Buck-AI-Guide, your undergrad CSE research advisor. I know everything about the OSU CSE program. Ask me anything!')
+st.title("🌰 💻 Buck-AI-Guide for CSE")
+st.info(
+    "Hi! I am Buck-AI-Guide, your undergrad CSE research advisor. I know everything about the OSU CSE program. Ask me anything!"
+)
 
 # Print the converstation history.
 for message in st.session_state.chat_history:
     if isinstance(message, HumanMessage):
-        with st.chat_message('Human'):
+        with st.chat_message("Human"):
             st.markdown(message.content)
     elif isinstance(message, AIMessage):
-        with st.chat_message('AI'):
-             st.markdown(message.content)
+        with st.chat_message("AI"):
+            st.markdown(message.content)
 
 # Get the next user query.
-user_query = st.chat_input('your message here')
-if user_query is not None and user_query != '':
+user_query = st.chat_input("your message here")
+if user_query is not None and user_query != "":
     st.session_state.chat_history.append(HumanMessage(user_query))
 
-    with st.chat_message('Human'):
+    with st.chat_message("Human"):
         st.markdown(user_query)
 
-    with st.chat_message('AI'):
-        response_stream, ref_urls = get_response_stream(user_query, st.session_state.chat_history)
+    with st.chat_message("AI"):
+        response_stream, ref_urls = get_response_stream(
+            user_query, st.session_state.chat_history
+        )
         ai_response = st.write_stream(response_stream)
 
         # Display the reference URLs.
-        st.markdown('#### Official Reference')
+        st.markdown("#### Official Reference")
         for url in ref_urls:
-            st.markdown(f'* [{url}]({url})')
+            st.markdown(f"* [{url}]({url})")
 
     st.session_state.chat_history.append(AIMessage(ai_response))
